69884501ab5dfc0e96f91bd218e23e0a7abfb860
jayvanput <jayvanput@gmail.com>
2020-06-20 13:49:37 -0400

    Added black squares to detail.html

--- crossword/static/crossword/js/detail.js
+++ crossword/static/crossword/js/detail.js
@@ -1,13 +1,21 @@
-const test = JSON.parse(JSON.parse(document.getElementById('squares').textContent))
+const squares = JSON.parse(JSON.parse(document.getElementById('squares').textContent))
 
 var btn = document.getElementById('check')
 var all_cells = document.getElementsByTagName("td")
+
+// Create answer array
 var answer = []
 
-for (let i = 0; i < test.length; i++) {
-  answer.push(test[i].fields.value)
+for (let i = 0; i < squares.length; i++) {
+  // Create black squares
+  if (squares[i].fields.value == "") {
+    all_cells[i].firstElementChild.style.backgroundColor = "black"
+  }
+  // Push to answer array
+  answer.push(squares[i].fields.value)
 }
 
+// Add win check functionality
 btn.addEventListener("click", function () {
   for (let i = 0; i < answer.length; i++) {
     if (answer[i] !== all_cells[i].firstElementChild.value) {
@@ -17,5 +25,3 @@ btn.addEventListener("click", function () {
   }
   alert('you win!')
 })
-
-
